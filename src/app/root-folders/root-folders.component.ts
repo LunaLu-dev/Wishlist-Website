3f2845530df548b06703fd6d59616538827e6c6b
Luna Nordbergh <luna.nordbergh2@gmail.com>
2023-10-22 22:59:56 +0200

    Added RootFolders and test images 	modified:   firebase-debug.log 	modified:   src/app/app.component.html 	modified:   src/app/root-folders/root-folders.component.css 	modified:   src/app/root-folders/root-folders.component.html 	modified:   src/app/root-folders/root-folders.component.ts 	modified:   src/main.ts

--- src/app/root-folders/root-folders.component.ts
+++ src/app/root-folders/root-folders.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root-folders',
@@ -9,7 +11,35 @@ import { Component } from '@angular/core';
 export class RootFoldersComponent {
 
   ngOnInit() {
-    
+    // Import the functions you need from the SDKs you need
+
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyD-21i_c71ZztSOOAVHg2Y2REK3031UzGM",
+      authDomain: "wishlist-website-b0f92.firebaseapp.com",
+      databaseURL: "https://wishlist-website-b0f92-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "wishlist-website-b0f92",
+      storageBucket: "wishlist-website-b0f92.appspot.com",
+      messagingSenderId: "1075962776143",
+      appId: "1:1075962776143:web:a9f688ac1125c7edfcf83a",
+      measurementId: "G-04H8TLJ8EK"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    this.test(app);
+
+
+
+  }
+
+  test(app :any) {
+    console.log(app)
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-21i_c71ZztSOOAVHg2Y2REK3031UzGM",
-  authDomain: "wishlist-website-b0f92.firebaseapp.com",
-  databaseURL: "https://wishlist-website-b0f92-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "wishlist-website-b0f92",
-  storageBucket: "wishlist-website-b0f92.appspot.com",
-  messagingSenderId: "1075962776143",
-  appId: "1:1075962776143:web:a9f688ac1125c7edfcf83a",
-  measurementId: "G-04H8TLJ8EK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-console.log(app);
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
