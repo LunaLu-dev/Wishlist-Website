4bb3f6da2da809caa5c0b8b0ded3f85f9948eda5
Luna Nordbergh <luna.nordbergh2@gmail.com>
2023-10-23 13:44:54 +0200

    Fixed Router 	modified:   .firebase/hosting.ZGlzdFx3aXNobGlzdC13ZWJzaXRl.cache 	new file:   firebase-debug.log 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/create-new/create-new.component.html 	modified:   src/app/create-new/create-new.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateNewComponent } from './create-new/create-new.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RootFoldersComponent } from './root-folders/root-folders.component';
 
 const routes: Routes = [
+  { path: '', component: RootFoldersComponent },
   { path: 'create', component: CreateNewComponent },
   { path: '**', component: PageNotFoundComponent}
 ];
--- src/app/create-new/create-new.component.ts
+++ src/app/create-new/create-new.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 @Component({
   selector: 'app-create-new',
   templateUrl: './create-new.component.html',
@@ -7,4 +13,8 @@ import { Component } from '@angular/core';
 })
 export class CreateNewComponent {
 
+
+  ngOnInit() {
+    console.log("Create Function works!!!");
+  }
 }
