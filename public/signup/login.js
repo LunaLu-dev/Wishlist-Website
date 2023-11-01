5915a953deee9975129d2bfd3a1f8c0d9694121d
Luna Nordbergh <luna.nordbergh2@gmail.com>
2023-11-01 06:12:48 +0100

    Added Login and Signup System 	modified:   chrome_ext/img/icon-128.png 	modified:   chrome_ext/img/icon-16.png 	modified:   chrome_ext/img/icon-48.png 	modified:   firebase.json 	modified:   public/404.html 	deleted:    public/404_Page_Not_Found.html 	new file:   public/category.html 	renamed:    public/folder.js -> public/category.js 	new file:   public/img/favicon-add-white (128x128).png 	new file:   public/img/favicon-add-white (16x16).png 	new file:   public/img/favicon-add-white (48x48).png 	new file:   public/login/index.html 	new file:   public/login/login.js 	new file:   public/signup/index.html 	new file:   public/signup/login.js

---
+++ public/signup/login.js
@@ -0,0 +1,32 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyD-21i_c71ZztSOOAVHg2Y2REK3031UzGM",
+        authDomain: "wishlist-website-b0f92.firebaseapp.com",
+        databaseURL: "https://wishlist-website-b0f92-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "wishlist-website-b0f92",
+        storageBucket: "wishlist-website-b0f92.appspot.com",
+        messagingSenderId: "1075962776143",
+        appId: "1:1075962776143:web:a9f688ac1125c7edfcf83a",
+        measurementId: "G-04H8TLJ8EK"
+});
+const auth = getAuth(firebaseApp);
+
+const loginEmailPassword = async () => {
+
+    const loginEmail = document.getElementById('login-email').value;
+    const loginPassword = document.getElementById('login-password').value;
+
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+    }catch (error){
+        console.error("An Error Occured");
+        document.getElementById('login-email').style.borderColor = "#ff0000";
+        document.getElementById('login-password').style.borderColor = "#ff0000";
+        document.getElementById('Status_Text').style.display = "block";
+    }
+}
+
+document.getElementById('login_btn').addEventListener("click", loginEmailPassword);
